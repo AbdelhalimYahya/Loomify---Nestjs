3cad0b89e640e36fb1c87254015ce2727a38d530
abdelhalim <abdelhalim@gmail.com>
2025-10-05 15:52:41 +0300

    adding the starting file of the user

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import ormconfig from './ormconfig'
 import { TagModule } from './tag/tag.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormconfig) , TagModule],
+  imports: [TypeOrmModule.forRoot(ormconfig) , TagModule , UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
